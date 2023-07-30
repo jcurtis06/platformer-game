080bb5f4e97aee1e8210e377eb47759a4be72405
Jonathan Curtis <06jcurtis@gmail.com>
2023-07-30 12:32:58 -0500

    updated to new rendering system

--- core/src/io/jcurtis/platformer/MainGame.kt
+++ core/src/io/jcurtis/platformer/MainGame.kt
@@ -3,36 +3,48 @@ package io.jcurtis.platformer
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.assets.AssetManager
+import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.FrameBuffer
 import com.badlogic.gdx.maps.tiled.TiledMap
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer
 import com.badlogic.gdx.maps.tiled.TmxMapLoader
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
-import com.badlogic.gdx.math.Vector3
-import com.badlogic.gdx.utils.ScreenUtils
 import com.badlogic.gdx.utils.viewport.FitViewport
 import com.badlogic.gdx.utils.viewport.Viewport
 import io.jcurtis.platformer.entities.Player
 import io.jcurtis.platformer.managers.CollisionManager
 import io.jcurtis.platformer.managers.EntityManager
 import io.jcurtis.platformer.utils.BoundingBox
-import kotlin.math.roundToInt
 
 object MainGame : ApplicationAdapter() {
-    var worldWidth = 1600;
-    var worldHeight = 1200;
+    // The virtual resolution of the game in pixels
+    // Lower values will zoom in, higher values will zoom out
+    const val VIRTUAL_WIDTH = 320
+    const val VIRTUAL_HEIGHT = 180
 
-    var viewWidth = 160f;
-    var viewHeight = 120f;
-
-    var camera: OrthographicCamera? = null
-    var fbo: FrameBuffer? = null
-    var viewport: Viewport? = null
     var batch: SpriteBatch? = null
+
+    // The texture and sprite used to render the player
+    var img: Texture? = null
+    var sprite: Sprite? = null
+
+    // The FrameBuffer used to scale the game to the screen
+    var frameBuffer: FrameBuffer? = null
+
+    // The camera used to render the scene. Do NOT move this camera around.
+    // It will always look at (VIRTUAL_WIDTH/2, VIRTUAL_HEIGHT/2) and have the same size as the virtual screen
+    var fboCamera: OrthographicCamera? = null
+
+    // the camera used within the game
+    var camera: OrthographicCamera? = null
+
+    // The viewport used to scale the game to the screen
+    var viewport: Viewport? = null
 
     var assetManager: AssetManager? = null
 
@@ -42,16 +54,23 @@ object MainGame : ApplicationAdapter() {
     var player: Player? = null
 
     override fun create() {
-        camera = OrthographicCamera()
-        camera!!.setToOrtho(false, worldWidth.toFloat(), worldHeight.toFloat())
-
-        viewport = FitViewport(viewWidth, viewHeight, camera)
-
-        fbo = FrameBuffer(Pixmap.Format.RGBA8888, worldWidth, worldHeight, false)
-        fbo!!.colorBufferTexture.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
-
         batch = SpriteBatch()
+        img = Texture("slime.png")
 
+        // Create both cameras
+        fboCamera = OrthographicCamera()
+        camera = OrthographicCamera()
+        camera!!.setToOrtho(false, VIRTUAL_WIDTH.toFloat(), VIRTUAL_HEIGHT.toFloat())
+
+        // Create the viewport
+        viewport = FitViewport(VIRTUAL_WIDTH.toFloat(), VIRTUAL_HEIGHT.toFloat(), fboCamera)
+        viewport!!.apply()
+
+        // Create a new FrameBuffer with a specific resolution
+        frameBuffer = FrameBuffer(Pixmap.Format.RGB888, VIRTUAL_WIDTH, VIRTUAL_HEIGHT, false)
+        frameBuffer!!.colorBufferTexture.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest)
+
+        // Load in assets
         assetManager = AssetManager()
         assetManager!!.setLoader(TiledMap::class.java, TmxMapLoader())
 
@@ -59,17 +78,18 @@ object MainGame : ApplicationAdapter() {
         assetManager!!.load("maps/test.tmx", TiledMap::class.java)
         assetManager!!.finishLoading()
 
+        // Create the player
         player = Player()
-
         EntityManager.add(player!!)
+
+        // Load the map
         map = assetManager!!.get("maps/test.tmx", TiledMap::class.java)
         mapRenderer = OrthogonalTiledMapRenderer(map, 1f)
         mapRenderer!!.setView(camera)
 
-        // add tilemap collisions
+        // Load the collision map
         val layer = map!!.layers.get(0) as TiledMapTileLayer
 
-        // loop through each tile in the layer
         for (x in 0 until layer.width) {
             for (y in 0 until layer.height) {
                 val cell = layer.getCell(x, y)
@@ -84,45 +104,58 @@ object MainGame : ApplicationAdapter() {
     }
 
     override fun render() {
+        // Update entities
         EntityManager.update(Gdx.graphics.deltaTime)
-        fbo!!.begin()
-        ScreenUtils.clear(1f, 1f, 1f, 1f)
 
-        batch!!.begin()
+        // Move the camera to the player
+        camera!!.position.x = player!!.position.x + 16 / 2
+        camera!!.position.y = player!!.position.y + 16 / 2
+
+        // Always update the camera after changing its position
+        camera!!.update()
+        mapRenderer!!.setView(camera)
+
+        // Start rendering to the FrameBuffer
+        frameBuffer!!.begin()
+
+        // Clear the screen with a white color
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        // Set the projection matrix to the camera's combined matrix
         batch!!.projectionMatrix = camera!!.combined
 
-        mapRenderer!!.render()
+        // Begin the batch
+        batch!!.begin()
+        // Everything in here will be rendered to the FrameBuffer
+        // Thus making it pixel-perfect
 
         if (assetManager!!.update()) {
             EntityManager.render(batch!!)
+            mapRenderer!!.render()
         }
 
-        val lerp = 0.1f
-
-        val targetPosition = Vector3(
-            player!!.position.x.roundToInt().toFloat(),
-            player!!.position.y.roundToInt().toFloat(),
-            0f
-        )
-
-        val newPosition = camera!!.position.lerp(targetPosition, lerp)
-
-        camera!!.position.set(newPosition)
-        camera!!.update()
-
+        // End the batch
         batch!!.end()
-        fbo!!.end()
 
+        // Stop rendering to the FrameBuffer
+        frameBuffer!!.end()
+
+        // Clear the screen with a black color
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         viewport!!.apply()
-        batch!!.projectionMatrix = camera!!.combined
+        batch!!.projectionMatrix = fboCamera!!.combined
+        fboCamera!!.setToOrtho(false, VIRTUAL_WIDTH.toFloat(), VIRTUAL_HEIGHT.toFloat())
 
+        // Render the FrameBuffer to the screen, scaling it to the screen's resolution
         batch!!.begin()
         batch!!.draw(
-            fbo!!.colorBufferTexture,
+            frameBuffer!!.colorBufferTexture,
             0f,
             0f,
-            worldWidth.toFloat(),
-            worldHeight.toFloat(),
+            viewport!!.worldWidth,
+            viewport!!.worldHeight,
             0f,
             0f,
             1f,
@@ -133,14 +166,12 @@ object MainGame : ApplicationAdapter() {
 
     override fun resize(width: Int, height: Int) {
         viewport!!.update(width, height)
-        camera!!.update()
+        fboCamera!!.update()
     }
 
     override fun dispose() {
         batch!!.dispose()
-        fbo!!.dispose()
-        assetManager!!.dispose()
-        map!!.dispose()
-        mapRenderer!!.dispose()
+        img!!.dispose()
+        frameBuffer!!.dispose()
     }
 }
--- desktop/src/io/jcurtis/platformer/DesktopLauncher.java
+++ desktop/src/io/jcurtis/platformer/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(MainGame.VIRTUAL_WIDTH * 4, MainGame.VIRTUAL_HEIGHT * 4);
 		config.setForegroundFPS(60);
 		config.setTitle("LibGDXPlatformer");
 		new Lwjgl3Application(MainGame.INSTANCE, config);
